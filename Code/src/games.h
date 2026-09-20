ae861ae9a2a379684255d8b6db8a2826f104e4f8
Nathan S <64860737+nathans-systems@users.noreply.github.com>
2026-09-20 15:58:30 +0100

    removing old code to insert bug fix

--- Code/src/games.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "watch.h"
-#include "buttons.h"
-
-void pongEnter();   void pongTick(BtnEvent ev);
-void snakeEnter();  void snakeTick(BtnEvent ev);
-void dinoEnter();   void dinoTick(BtnEvent ev);
